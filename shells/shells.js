d1924e8ebac8405134ed179b1227fb9b0cb4277e
Josh Borrow <joshua.borrow@durham.ac.uk>
2016-08-31 13:22:50 +0100

    minor bugfix

--- shells/shells.js
+++ shells/shells.js
@@ -12,13 +12,13 @@ function getSVGById(id) {
 	return container.contentDocument;
 };
 
-function changeProtons(protonSVG, numToChange, color, emptyColor='white') {
+function changeProtons(protonSVG, numToChange, color='red', emptyColor='white') {
 	for (i = 1; i <= numToChange; i++) {
 		thisProton = protonSVG.getElementById('p' + i);
 		thisProton.style.fill = color;
 	};
 
-	for (i = numToChange; i <= numberOfProtons; i++) { // to ensure using lower numbers displays correctly
+	for (i = numToChange+1; i <= numberOfProtons; i++) { // to ensure using lower numbers displays correctly
 		thisProton = protonSVG.getElementById('p' + i);
 		thisProton.style.fill = emptyColor;
 	};
@@ -27,10 +27,10 @@ function changeProtons(protonSVG, numToChange, color, emptyColor='white') {
 //initially, we must wait for the SVG to load NOT the document.
 $('#protons').on('load', function () {
 	protonSVG = getSVGById('protons');
-	changeProtons(protonSVG, 0, 'white');
+	changeProtons(protonSVG, 0);
 });
 
 $('#numProtons').on('input', function () {
 	numProtons = $('#numProtons').val();
-	changeProtons(protonSVG, numProtons, 'red');
+	changeProtons(protonSVG, numProtons);
 });
